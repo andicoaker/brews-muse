06d2678f33260e7751ff7d82aa1330060cc52e87
andicoaker <andicoaker@gmail.com>
2017-03-16 13:26:01 -0400

    started view-controller

---
+++ Application/src/Application.Web/src-client/scripts/view-controller.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Navbar} from './components/component-navbar.js';
+import {STORE} from './store.js';
+import {ACTIONS} from './actions.js';
+
+import {HomeView} from './views/view-home.js'
+import {AllVendorsView} from './views/view-all_vendors.js'
+import {RegisterView} from './views/view-register.js'
+import {LoginView} from './views/view-login.js'
+import {VendorAccountView} from './views/view-vendor_account.js'
+import {VendorProfileView} from './views/view-vendor_profile.js'

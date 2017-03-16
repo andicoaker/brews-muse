b91a89b128c5a6d0ee1ba01bb97e9833f2261518
andicoaker <andicoaker@gmail.com>
2017-03-16 15:13:36 -0400

    completed backbone router and working on view controller

--- Application/src/Application.Web/src-client/scripts/view-controller.js
+++ Application/src/Application.Web/src-client/scripts/view-controller.js
@@ -9,3 +9,59 @@ import {RegisterView} from './views/view-register.js'
 import {LoginView} from './views/view-login.js'
 import {VendorAccountView} from './views/view-vendor_account.js'
 import {VendorProfileView} from './views/view-vendor_profile.js'
+
+export const ViewController = React.createClass({
+
+  getInitialState: function(){
+    ACTIONS.changeCurrentNav(this.props.fromRoute, window.location.hash)
+    let storeObject = STORE.getStoreData()
+    return storeObject
+  },
+
+  componentDidMount: function(){
+		let component = this;
+
+	   STORE.onStoreChange(function(){
+			console.log("YAY CHANGED STATE!")
+
+			let newStoreObj = STORE.getStoreData()
+			component.setState(newStoreObj)
+		})
+		ACTIONS.fetchCurrentUser()
+	},
+
+  render: function(){
+
+    let componentToRender
+
+    switch(this.state.currentNavRoute){
+  			case "LOGIN":
+  				componentToRender = <LoginView {...this.state}/>
+  				break;
+  			case "REGISTER":
+  				componentToRender = <RegisterView {...this.state}/>
+  				break;
+   			case "ALL_VENDORS":
+  				componentToRender = <AllVendorsView {...this.state}/>
+  				break;
+        case "VENDOR_ACCOUNT":
+  				componentToRender = <VendorAccountView {...this.state}/>
+  				break;
+        case "VENDOR_PROFILE":
+  				componentToRender = <VendorProfileView {...this.state}/>
+  				break;
+        case "HOME":
+  				componentToRender = <HomeView {...this.state}/>
+  				break;
+   			default:
+  	}
+
+    return (
+			<div>
+
+				{componentToRender}
+			</div>
+		)
+
+  }
+})

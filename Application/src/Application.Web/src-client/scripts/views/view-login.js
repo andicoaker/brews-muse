10faed218e4fa9ea31db0296be0a3bdbfb83e0c5
Braden Holmes <bradenholmes92@bradens-mbp.attlocal.net>
2017-03-20 04:46:58 -0400

    yoyoyoyo

--- Application/src/Application.Web/src-client/scripts/views/view-login.js
+++ Application/src/Application.Web/src-client/scripts/views/view-login.js
@@ -2,8 +2,7 @@ import React from 'react'
 import {LoginComponent} from '../components/component-form_login.js'
 
 
-export const RegisterView = React.createClass({
-  console.log(RegisterView)
+export const LoginView = React.createClass({
   render: function(){
     return (
       <div className="login-container">

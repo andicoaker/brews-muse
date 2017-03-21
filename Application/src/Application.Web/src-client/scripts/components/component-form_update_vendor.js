36adc7bc8b0b5247c22bdb1765065c22da7c9c15
Braden Holmes <bradenholmes92@bradens-mbp.attlocal.net>
2017-03-21 19:08:57 -0400

    form stuff

--- Application/src/Application.Web/src-client/scripts/components/component-form_update_vendor.js
+++ Application/src/Application.Web/src-client/scripts/components/component-form_update_vendor.js
@@ -4,12 +4,43 @@ export const UpdateVendorComponent = React.createClass({
   render: function(){
     return (
       <form>
-        <div className="form-group">
-          <label className="control-label col-sm-2" for="email">Email:</label>
-          <div className="col-sm-10">
-            <input type="email" className="form-control" id="email" placeholder="Enter email"/>
+         <h1>Update Account</h1>
+          <div className="form-group">
+            <input type="text" className="form-control" id="inputFirstName" placeholder="First Name"/>
           </div>
-        </div>
+          <div className="form-group">
+            <input type="text" className="form-control" id="inputLastName" placeholder="Last Name"/>
+          </div>
+          <div className="form-group">
+              <input type="text" className="form-control" id="inputCompany" placeholder="Company"/>
+          </div>
+          <div className="form-group">
+              <input type="text" className="form-control" id="inputHours" placeholder="Hours"/>
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control" id="inputEmail" placeholder="Email"/>
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control" id="inputPhone" placeholder="Phone"/>
+          </div>
+          <div className="form-group">
+              <input type="text" className="form-control" id="inputAddress" placeholder="Address"/>
+          </div>
+          <div className="form-group">
+              <input type="text" className="form-control" id="inputCity" placeholder="City"/>
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control" id="inputState" placeholder="ST"/>
+          </div>
+          <div className="form-group">
+              <input type="text" className="form-control" id="inputZip" placeholder="ZIP"/>
+          </div>
+          <div className="form-group">
+              <input type="text" className="form-control" id="inputWebsite" placeholder="Website http://"/>
+          </div>
+          <div className="form-group">
+              <button type="submit" className="btn btn-default">Login</button>
+          </div>
       </form>
     )
   }

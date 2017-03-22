99328fbfe26adf9978986138745529ba162fa5f3
Braden Holmes <bradenholmes92@Bradens-MacBook-Pro.local>
2017-03-22 15:19:15 -0400

    working on vendor profile

---
+++ Application/src/Application.Web/src-client/scripts/components/component-vendor_details.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const VendorDetailsComponent = React.createClass({
+  render: function(){
+    return (
+      <div className="row">
+          <div className="thumbnail">
+            <img src="..." alt="..."/>
+            <div className="caption">
+              <h3>Thumbnail label</h3>
+              <p>...</p>
+            </div>
+          </div>
+      </div>
+    )
+  }
+})

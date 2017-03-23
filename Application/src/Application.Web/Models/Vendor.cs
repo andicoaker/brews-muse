9c3e646a69deb43c5b06f66338ce0f8bbbb347a6
ptank <kptankersley@gmail.com>
2017-03-23 11:31:34 -0400

    simplified data models and added some more dummy data

--- Application/src/Application.Web/Models/Vendor.cs
+++ Application/src/Application.Web/Models/Vendor.cs
@@ -13,8 +13,8 @@ namespace BrewsMuse.Models
 
         public ApplicationUser Owner { get; set; }
         public string OwnerName { get; set; }
-        public string Address1 { get; set; }
-        public string Address2 { get; set; }
+        public string Address { get; set; }
+        //public string Address2 { get; set; }
         public string City { get; set; }
         public string State { get; set; }
         public int ZipCode { get; set; }
@@ -22,14 +22,11 @@ namespace BrewsMuse.Models
         public string VendorURL { get; set; }
         public string ImageURL { get; set; }
         public string Comments { get; set; }
-        public int VendorId { get; set; }
+        //public int VendorId { get; set; }
         public string VendorPhone { get; set; }
         public int Vote { get; set; }
         public int CheckIn { get; set; }
-        //public DateTime Hours { get; set; } //need start and end instead of hours. maybe timespan
-        //public TimeSpan Hours { get; set; }
-        public string OpeningTIme { get; set; }
-        public string ClosingTime { get; set; }
+        public string Hours { get; set; }        
         public double Lat { get; set; }
         public double Lng { get; set; }
         public List<Beer> Beers { get; set; }

ae3b626b42939e01e48ff39ced598aef445c4a3f
andicoaker <andicoaker@gmail.com>
2017-03-25 12:06:18 -0400

    testing pull/push

--- Application/src/Application.Web/src-client/scripts/components/component-map.js
+++ Application/src/Application.Web/src-client/scripts/components/component-map.js
@@ -4,18 +4,44 @@ import GoogleMapReact from 'google-map-react'
 
 export const MapComponent = React.createClass({
 
+  // getInitialState: function(){
+  //   return{
+  //     locationsData : this.props.locationsData
+  //   }
+  //
+  // }
+
   _createMapPins: function(locationsArray){
-		let mapPinComponents= locationsArray.map(function(locationObj, i){
+    let component = this
+    let mapPinComponents= locationsArray.map(function(locationObj, i){
       // console.log(locationObj);
       // console.log(locationsArray);
 
       return (
-        <MapPin key={Date.now()+i} lat={locationObj.lat} lng={locationObj.lng} vendorName={locationObj.name} />
+        <MapPin key={Date.now()+i} lat={locationObj.lat} lng={locationObj.lng} vendorName={locationObj.name} handlePinHoverCB ={this._handlePinHoverCB}/>
       )
 		})
 
 		return mapPinComponents
 	},
+
+  // _handleMapClick: function(evt){
+  //   console.log(evt);
+  //   let locationsCopy = [...this.state.locationsData]
+  //   locationsCopy.push({lt: evt.lat, lg: evt.lng})
+  //
+  //   this.setState({
+  //     locationsData : locationsCopy
+  //   })
+  //
+  // }
+
+  _handlePinHoverCB: function(payload){
+    console.log(POLO!, payload);
+    this.props.handlePinHoverCB(this.props.place)
+
+    this.setState()
+  }
 
   render: function(){
     return (
@@ -23,18 +49,37 @@ export const MapComponent = React.createClass({
       <div className="map-container">
         <GoogleMapReact defaultZoom={14} defaultCenter={{lat: 32.784618, lng: -79.940918}}>
           {this._createMapPins(this.props.locationsData)}
+          <h3>
+            Location Info: <mark></mark>
+          </h3>
         </GoogleMapReact>
-
       </div>
     )
   },
 })
 
+
+
 const MapPin = React.createClass({
+
+  getInitialState: function(){
+    pinStyle:
+  }
+
+  _togglePinHover: function (evt){
+    console.log('hello? is it me you are listening for?', evt);
+
+    // if(evt.type === 'mouseover')
+    console.log('MARCO: ', thisprops.place);
+
+
+  }
+
+
 
 	render: function(){
 		return (
-			<div>
+			<div onMouseOver={this._togglePinHover} onMouseOut={this._togglePinHover}>
         <span>{this.props.vendorName}</span>
         <i className="fa fa-map-marker" aria-hidden="true"></i>
 			</div>
